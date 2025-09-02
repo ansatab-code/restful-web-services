3514452ef83b2b5e83c5fd9025805bc7bf48c254
Ansata Baldé <ansata.sy@gmail.com>
2025-09-02 18:44:00 +0200

    delete Resource

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,11 @@ public class UserResource {
 		return ResponseEntity.created(location).body(user);
 		
 	}
+	
+	// /GET /users
+		@DeleteMapping(path = "/users/{id}")
+		public void deleteUser(@PathVariable Integer id ){
+			userDaoService.deleteById(id);
+		}
 	
 }
