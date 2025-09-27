d8a0751ff29493ffafa7204dd99e321f82bdb113
Ansata Baldé <ansata.sy@gmail.com>
2025-09-27 07:39:26 +0200

    i18n

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 //REST API
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController (MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	// /hello-word
 	
 	// "Hello Word"
@@ -29,5 +39,13 @@ public class HelloWorldController {
 		
 		return new HelloWordBean(String.format("Hello Word , %s", name));
 	}
+	
+	// "Hello Word"
+		@GetMapping(path="/hello-world-internationalized")
+		public String helloWordInternationalized (){
+			Locale locale= LocaleContextHolder.getLocale();
+			return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+			//return "Hello World V2";
+		}
 
 }
