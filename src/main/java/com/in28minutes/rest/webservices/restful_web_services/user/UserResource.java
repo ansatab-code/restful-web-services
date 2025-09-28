3c4bea33d2a6fd9ec0bf3a004d0736c7b8bef7da
Ansata Baldé <ansata.sy@gmail.com>
2025-09-28 10:53:23 +0200

    hateoas

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,12 +34,15 @@ public class UserResource {
 	
 	// /GET /users
 	@GetMapping(path = "/users/{id}")
-	public User retrieveUser(@PathVariable Integer id ){
+	public EntityModel<User> retrieveUser(@PathVariable Integer id ){
 		User user = userDaoService.findOne(id);
 		if (user == null) {
 			throw new UserNotFoundException("id:"+id);
 		}
-		return user;
+		EntityModel<User>  entityModel = EntityModel.of(user);
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
 	
 	// /POST /users
